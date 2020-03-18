540a519cfacd7887293b1d63051386ce31937650
Laura Cressman <lauracressman@mac.com>
2020-03-18 17:00:17 -0600

    Use full inquirer instead of alpha sub packages

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,13 +1,21 @@
-import confirm from '@inquirer/confirm';
-import select from '@inquirer/select';
+import inquirer from 'inquirer';
 import { pathExists } from 'fs-extra';
 import { CI_PROVIDERS } from './providers';
 
-export const promptConfirmOverwrite = (path: string): Promise<boolean> =>
-  confirm({
-    message: `"${path}" already exists, overwrite it?`,
-    default: false,
-  });
+export const promptConfirmOverwrite = async (
+  path: string,
+): Promise<boolean> => {
+  const answers = await inquirer.prompt([
+    {
+      default: false,
+      message: `"${path}" already exists, overwrite it?`,
+      name: 'overwrite',
+      type: 'confirm',
+    },
+  ]);
+
+  return answers.overwrite;
+};
 
 export const promptOverwrite = async (path: string): Promise<boolean> => {
   const exists = await pathExists(path);
@@ -17,10 +25,16 @@ export const promptOverwrite = async (path: string): Promise<boolean> => {
 };
 
 export const promptCiProvider = async (): Promise<string | null> => {
-  return select({
-    message: 'Select a CI provider',
-    choices: CI_PROVIDERS.map(provider => {
-      return { name: provider.label, value: provider.name };
-    }),
-  });
+  const answers = await inquirer.prompt([
+    {
+      choices: CI_PROVIDERS.map(provider => {
+        return { name: provider.label, value: provider.name };
+      }),
+      message: 'Choose CI Provider',
+      name: 'provider',
+      type: 'list',
+    },
+  ]);
+
+  return answers.provider;
 };
