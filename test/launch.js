b82116a8f2a0ae1f7c6c288318a3000d5d9dc740
Jon Perl <perl.jonathan@gmail.com>
2020-03-25 10:27:04 -0600

    process.exit(1) on launch failure

--- test/launch.js
+++ test/launch.js
@@ -1,15 +1,20 @@
 const pw = require('playwright');
 
 (async () => {
-  let browser = await pw.chromium.launch({ args: ['--no-sandbox'] });
-  console.log('launched chromium!');
-  await browser.close();
+  try {
+    let browser = await pw.chromium.launch({ args: ['--no-sandbox'] });
+    console.log('launched chromium!');
+    await browser.close();
 
-  browser = await pw.firefox.launch();
-  console.log('launched firefox!');
-  await browser.close();
+    browser = await pw.firefox.launch();
+    console.log('launched firefox!');
+    await browser.close();
 
-  browser = await pw.webkit.launch();
-  console.log('launched webkit!');
-  await browser.close();
+    browser = await pw.webkit.launch();
+    console.log('launched webkit!');
+    await browser.close();
+  } catch (error) {
+    console.log('error launching browsers', error);
+    process.exit(1);
+  }
 })();
